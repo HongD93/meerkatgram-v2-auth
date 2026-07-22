4ece194e93de22f1ca1b7aa22885396245377fff
HongD93 <tigerson93@gmail.com>
2026-07-23 07:29:42 +0900

    add login & security & cookie & jwt complete

--- src/main/java/com/meerkatgramv2auth/domain/auth/service/AuthService.java
+++ src/main/java/com/meerkatgramv2auth/domain/auth/service/AuthService.java
@@ -1,8 +1,13 @@
 package com.meerkatgramv2auth.domain.auth.service;
 
-import com.meerkatgramv2auth.domain.auth.request.SignupRequestDTO;
+import com.meerkatgramv2auth.domain.auth.repository.AuthRepository;
+import com.meerkatgramv2auth.domain.auth.request.LoginRequestDTO;
+import com.meerkatgramv2auth.domain.auth.response.AuthResponseDTO;
 import com.meerkatgramv2auth.domain.user.entity.User;
-import com.meerkatgramv2auth.domain.user.repository.UserRepository;
+import com.meerkatgramv2auth.global.cookie.CookieManager;
+import com.meerkatgramv2auth.global.error.custom.NotRegisteredException;
+import com.meerkatgramv2auth.global.jwt.JwtProvider;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,22 +16,37 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-
-    private final UserRepository userRepository;
+    private final AuthRepository authRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
+    private final CookieManager cookieManager;
 
-    @Transactional
-    public void signup(SignupRequestDTO requestDTO) {
-        if (userRepository.existsByEmail(requestDTO.email())) {
-            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+    @Transactional(rollbackFor = Exception.class)
+    public AuthResponseDTO login(HttpServletResponse response, LoginRequestDTO loginRequestDTO) {
+        // 유저 정보를 획득 & 유저 가입 여부를 체크
+        User user = authRepository.findByEmail(loginRequestDTO.email())
+            .orElseThrow(() -> new NotRegisteredException("아이디와 비밀번호를 확인해주세요."));
+
+        // 비밀번호 체크
+        if(!passwordEncoder.matches(loginRequestDTO.password(), user.getPassword())) {
+            throw new NotRegisteredException("아이디와 비밀번호를 확인해주세요.");
         }
 
-        User user = User.builder()
-            .email(requestDTO.email())
-            .password(passwordEncoder.encode(requestDTO.password()))
-            .nickname(requestDTO.nickname())
-            .build();
+        return this.generateAuthentication(response, user);
+    }
 
-        userRepository.save(user);
+    private AuthResponseDTO generateAuthentication(HttpServletResponse response, User user) {
+        // 토큰생성
+        String accessToken = jwtProvider.generateAccessToken(user);
+        String refreshToken = jwtProvider.generateRefreshToken(user);
+
+        // 리프래시토큰 DB 저장 처리
+        user.setRefreshToken(refreshToken);
+        authRepository.save(user);
+
+        // 리프래시 토큰 cookie에 저장
+        cookieManager.setRefreshTokenToCookie(response, refreshToken);
+
+        return AuthResponseDTO.from(user, accessToken);
     }
 }
--- src/main/java/com/meerkatgramv2auth/domain/user/entity/User.java
+++ src/main/java/com/meerkatgramv2auth/domain/user/entity/User.java
@@ -1,46 +1,66 @@
 package com.meerkatgramv2auth.domain.user.entity;
 
+import com.meerkatgramv2auth.global.security.constant.ProviderPolicy;
+import com.meerkatgramv2auth.global.security.constant.RolePolicy;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.sql.Types;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "users")
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
+@Table(name = "users")
+@SQLDelete(sql = "UPDATE users SET deleted_at = NOW() WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
+@Getter
+@Setter
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 자동 생성 전략 설정
+    @Column(name = "id", columnDefinition = "BIGINT UNSIGNED")
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "email", unique = true, nullable = false, length = 100)
     private String email;
 
-    @Column(nullable = false)
+    @Column(name = "password", nullable = false, length = 255)
     private String password;
 
-    @Column(nullable = false)
-    private String nickname;
+    @Column(name = "nick", nullable = false, length = 20)
+    private String nick;
 
-    @CreatedDate
-    @Column(updatable = false)
+    @Column(name = "provider", nullable = false, length = 10)
+    @Enumerated(value = EnumType.STRING) // Enum을 어떤 데이터형식으로 저장할 건지 설정
+    @JdbcTypeCode(Types.VARCHAR)
+    private ProviderPolicy provider = ProviderPolicy.NONE;
+
+    @Column(name = "role", nullable = false, length = 10)
+    @Enumerated(value = EnumType.STRING)
+    @JdbcTypeCode(Types.VARCHAR)
+    private RolePolicy role = RolePolicy.NORMAL;
+
+    @Column(name = "profile", nullable = false, length = 255)
+    private String profile;
+
+    @Column(name = "refresh_token", nullable = true, length = 255)
+    private String refreshToken;
+
+    @CreatedDate // 생성 시 자동으로 시간 입력
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    @LastModifiedDate
+    @LastModifiedDate // 수정 시 자동으로 시간 업데이트
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
-    @Builder
-    public User(String email, String password, String nickname) {
-        this.email = email;
-        this.password = password;
-        this.nickname = nickname;
-    }
+    @Column(name = "deleted_at", nullable = true)
+    private LocalDateTime deletedAt;
 }
