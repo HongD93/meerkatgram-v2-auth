291b53e704c8866c5dbe29932ecf6835c4319920
HongD93 <tigerson93@gmail.com>
2026-07-27 11:14:25 +0900

    edt GlobalExceptionHandler: Logging bug(Invalid parameter errors not write error info)

--- src/main/java/com/meerkatgramv2auth/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/meerkatgramv2auth/global/error/GlobalExceptionHandler.java
@@ -69,7 +69,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<GlobalResponseDTO<Void>> methodArgumentTypeMismatchHandle(MethodArgumentTypeMismatchException e) {
-        log.debug(CustomResponseCode.INVALID_PARAMETER_ERROR.name(), String.format("%s : 필드를 확인해 주세요.", e.getName()));
+        log.debug("{}\n{}", CustomResponseCode.INVALID_PARAMETER_ERROR.name(), String.format("%s : 필드를 확인해 주세요.", e.getName()));
         return this.generateErrorResponse(CustomResponseCode.INVALID_PARAMETER_ERROR);
     }
 
@@ -84,7 +84,7 @@ public class GlobalExceptionHandler {
                 (existing, replacement) -> existing // 중복 필드가 있을 경우 기존 값 유지
             ));
 
-        log.debug(CustomResponseCode.INVALID_PARAMETER_ERROR.name(), errors);
+        log.debug("{}\n{}", CustomResponseCode.INVALID_PARAMETER_ERROR.name(), errors);
         return this.generateErrorResponse(CustomResponseCode.INVALID_PARAMETER_ERROR);
     }
 
